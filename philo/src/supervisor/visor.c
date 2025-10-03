1ac8baa8fee417174984b2bcb3549fcb8afa33bd
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-10-03 14:16:12 +0200

    Add to supervisor monitor of life of philo

--- philo/src/supervisor/visor.c
+++ philo/src/supervisor/visor.c
@@ -6,12 +6,13 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 17:26:33 by nluchini          #+#    #+#             */
-/*   Updated: 2025/10/02 20:53:27 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/10/03 14:15:47 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_internal.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 // Should be run in a separate thread
@@ -27,62 +28,50 @@ static void	print_message(t_event event)
 		printf("%lu %d has taken a fork\n", event.timestamp, event.philo_id);
 }
 
-// static void	__print_if_changed_status(t_data *data)
-// {
-// 	t_event	events[MAX_SIZE];
-// 	int		is_new_event;
-// 	int		i;
-// 	int		j;
-
-// 	is_new_event = 0;
-// 	i = 0;
-// 	pthread_mutex_lock(&data->queue->lock);
-// 	while (!is_empty(data->queue))
-// 	{
-// 		is_new_event = 1;
-// 		events[i] = peek(data->queue);
-// 		if (events[i].type != IS_EMPTY)
-// 			det_queue(data->queue);
-// 		i++;
-// 	}
-// 	pthread_mutex_unlock(&data->queue->lock);
-// 	j = -1;
-// 	if (is_new_event)
-// 	{
-// 		while (++j < i)
-// 			print_message(events[j]);
-// 	}
-// }
-
-static void	print_if_changed_status(t_data *data)
+static int	handle_events(t_data *data, t_life_time *life_times)
 {
-	int	n;
+	t_event	batch[MAX_SIZE];
+	int		n;
+	int		i;
 
-	t_event batch[MAX_SIZE]; // small stack buffer
-	n = 0;
-	pthread_mutex_lock(&data->queue->lock);
-	while (!is_empty(data->queue))
+	n = get_event(data, batch, MAX_SIZE);
+	if (n == 0)
+		return (1);
+	i = -1;
+	while (++i < n)
 	{
-		batch[n++] = peek(data->queue);
-		det_queue(data->queue);
-	}
-	pthread_mutex_unlock(&data->queue->lock);
-	for (int i = 0; i < n; i++)
+		if (batch[i].type == END_ITER)
+			return (0);
+		if (batch[i].type == PHILO_EATING)
+			update_meal_time(life_times, batch[i].philo_id, batch[i].timestamp
+				+ data->start_time);
 		print_message(batch[i]);
+	}
+	return (1);
 }
 
 int	run_supervisor(void *arg)
 {
-	t_data	*data;
+	t_data		*data;
+	t_life_time	*life_times;
 
 	data = (t_data *)arg;
 	if (!data)
 		return (1);
-	usleep(300);
+	if (!init_life_times(data, &life_times))
+		return (set_death(data, 1), 1);
+	usleep(200);
 	while (1)
 	{
-		print_if_changed_status(data);
+		if (update_dead_status_if_dead(data, life_times))
+		{
+			printf("%lu %d died\n", get_delta_ms(data), life_times[0].id);
+			break ;
+		}
+		if (!handle_events(data, life_times))
+			break ;
 		usleep(1000);
 	}
+	free(life_times);
 	return (0);
 }
