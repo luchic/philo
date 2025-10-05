8876940019cfd0facba8f809470fb15ba21fcf66
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-10-05 19:52:44 +0200

    Update philo

--- philo/src/actions/action.c
+++ philo/src/actions/action.c
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 20:54:18 by nluchini          #+#    #+#             */
-/*   Updated: 2025/10/05 18:46:58 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/10/05 19:45:18 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,12 @@ void	run_action(void *arg)
 	philo = (t_philo *)arg;
 	philo->iterations = 0;
 	if (philo->id % 2 != 0)
-		(usleep(200), thinking(philo));
-	if (philo->data->count % 2 != 0 && philo->data->count - 1== philo->id)
-		(usleep(200), thinking(philo));
-	while (1)
+		(usleep(50));
+	while (get_dead_status(philo->data) == 0)
 	{
+		thinking(philo);
+		if (get_dead_status(philo->data))
+			return ;
 		philo->iterations++;
 		if (eating(philo))
 			return ;
@@ -63,9 +64,6 @@ void	run_action(void *arg)
 		if (philo->data->max_iter > 0
 			&& philo->iterations >= philo->data->max_iter)
 			break ;
-		thinking(philo);
-		if (get_dead_status(philo->data))
-			return ;
 	}
 	end_rutine(philo);
 }
