a0966e4f6e92babc4ebda3e28b6ac10664e4d326
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-10-05 13:56:16 +0200

    Handle one philo

--- philo/src/actions/eating.c
+++ philo/src/actions/eating.c
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 18:19:46 by nluchini          #+#    #+#             */
-/*   Updated: 2025/10/04 13:41:14 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/10/05 13:00:00 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,15 @@ static void	is_fork_taken(t_philo *philo)
 
 static void	lock_eating(t_philo *philo)
 {
+	if (philo->data->count == 1)
+	{
+		pthread_mutex_lock(philo->left_fork);
+		is_fork_taken(philo);
+		while (!get_dead_status(philo->data))
+			usleep(100);
+		pthread_mutex_unlock(philo->left_fork);
+		return ;
+	}
 	if (philo->id % 2 == 0)
 	{
 		pthread_mutex_lock(philo->right_fork);
