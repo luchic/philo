04d848a9976618ab182918a2625f0252c9ad8b03
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-10-10 10:55:49 +0200

    Update sleep

--- philo/src/utils/sleep.c
+++ philo/src/utils/sleep.c
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 11:14:23 by nluchini          #+#    #+#             */
-/*   Updated: 2025/10/05 13:20:01 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/10/09 09:45:16 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,10 @@ static unsigned long	get_deadline_ms(t_philo *philo, unsigned long ms)
 
 	ms_unit = ms % 10;
 	cur_unit = get_delta_ms(philo->data) % 10;
-	if (ms_unit == 0)
+	if (ms_unit == 0 && philo->data->time_to_sleep % 10 == 0
+		&& philo->data->time_to_eat % 10 == 0)
+		allignment = cur_unit % 10;
+	else if (ms_unit == 0)
 		allignment = 0;
 	else
 		allignment = cur_unit % ms_unit;
